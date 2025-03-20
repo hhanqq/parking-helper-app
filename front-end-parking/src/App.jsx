46ff02fa74942c67a307607d0426e09545dd2e57
kvintosis <kvitosis55887@mail.ru>
2025-03-20 10:43:18 +0700

    header without styles, directory update

---
+++ front-end-parking/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Header from './components/Header';
+
+function App() {
+    return (
+        <Router>
+            <Header />
+        </Router>
+    );
+}
+
+export default App;
